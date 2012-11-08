91273c0201108cedd5614a9abf662fa457a0a8ae
Daniel Gamboa <danielgamboa10@gmail.com>
2012-11-07 17:11:04 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shortenit::Application.config.secret_token = '6f9e289678e9ab53a7b1d490560ef13d55d45608551124eacebc73cb1f2da52ccd4ccffd3f2f56d26bfae3bc2d2922d60c3d6e32808284f9c6aff79c21d0a1f0'
